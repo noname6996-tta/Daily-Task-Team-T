974a945a1b202810f44da6f903c00f8ba234f43e
Bui Nhat <buinhat408@gmail.com>
2025-01-04 10:15:46 +0700

    Add demo base adapter

--- app/src/main/java/com/tta/dailytaskteamt/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/tta/dailytaskteamt/ui/menu/MenuFragment.kt
@@ -3,10 +3,15 @@ package com.tta.dailytaskteamt.ui.menu
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.tta.core_base.BaseFragment
+import com.tta.core_utils.extension.toast
 import com.tta.dailytaskteamt.databinding.FragmentMenuBinding
+import com.tta.dailytaskteamt.ui.menu.adapter.TestAdapter
 
 
 class MenuFragment(override var isTerminalBackKeyActive: Boolean = false) : BaseFragment<FragmentMenuBinding>() {
+
+    private val testList = arrayListOf<String>()
+    private lateinit var testAdapter: TestAdapter
 
     override fun getDataBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentMenuBinding {
         return FragmentMenuBinding.inflate(inflater, container, false)
@@ -14,6 +19,15 @@ class MenuFragment(override var isTerminalBackKeyActive: Boolean = false) : Base
 
     override fun initView() {
         super.initView()
+
+        for (i in 0 until 30) {
+            testList.add(i.toString())
+        }
+        testAdapter = TestAdapter(testList)
+        binding.rcv.adapter = testAdapter
+        testAdapter.itemClickListener = { data, _ ->
+            activity?.toast("Data: $data")
+        }
     }
 
 }
